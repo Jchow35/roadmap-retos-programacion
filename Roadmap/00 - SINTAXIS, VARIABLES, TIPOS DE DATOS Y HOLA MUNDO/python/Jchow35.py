ac8011063abab32f09eb91c35746c6ac520783ff
Jchow35 <juancarlosmolinachow@gmail.com>
2024-08-03 18:49:56 -0600

    Primer ejercicio del curso

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jchow35.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jchow35.py
@@ -2,3 +2,49 @@
 
 # Comentario en una linea 
 
+"""
+Esto tambien Es
+un Comentario 
+en varias lineas
+"""
+
+'''
+Esto tambien es 
+un comentario 
+en varias lineas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONTANT = 'Mi constante' # por convencion
+
+print(my_variable)
+
+# Tipo de datos
+
+my_int = 1 # Tipo de variable de tipo entero
+my_float = 1.5 # Tipo de dato float 
+my_bool = True # tipo de dato boleano
+my_bool = False # tipo de dato boleano
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+# Uso de print para que se visualice lo que contiene las variables
+
+print("Hola, python") 
+
+print(my_int)
+print(my_float)
+print(my_bool)
+print(my_string)
+print(my_other_string)
+
+# Uso de Type para saber el tipo de dato 
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+print(type(my_other_string))
+
